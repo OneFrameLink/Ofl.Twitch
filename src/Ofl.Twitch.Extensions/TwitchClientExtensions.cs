fdf7a9824142b74acfe36c9448126a4d299e427b
casperOne <casperOne@caspershouse.com>
2020-02-18 21:15:01 -0500

    Fixed namespace for extensions.

--- src/Ofl.Twitch.Extensions/TwitchClientExtensions.cs
+++ src/Ofl.Twitch.Extensions/TwitchClientExtensions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Ofl.Twitch.Extensions
+namespace Ofl.Twitch
 {
     public static class TwitchClientExtensions
     {
